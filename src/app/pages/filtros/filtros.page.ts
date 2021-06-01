6c19b6fa1147c59e110f9b675498ffec7000be2c
Victor-droid165 <rodrigues.victor@academico.ifpb.edu.br>
2021-06-01 11:37:42 -0300

    Tela Filtros 2.1

--- src/app/pages/filtros/filtros.page.ts
+++ src/app/pages/filtros/filtros.page.ts
@@ -11,6 +11,17 @@ export class FiltrosPage {
   estados = [{nome: "Paraíba"}, { nome: "Acre"}];
   cidades = [{nome: "Campina Grande"}, { nome: "Rio Branco"}];
   faixasPrecos = [{minimo: 100, maximo: 1000},{minimo: 1000, maximo:5000}];
+  selected = [{},{},{},{}, {}];
   constructor() {}
-  
+
+  selectChanged(obj, indice:number){
+    this.selected[indice] = obj;
+  }
+
+  onFiltrar(){
+    this.selected.forEach(element => {
+      console.log(element);
+    });
+    alert("Filtrado com sucesso!");
+  }
 }
