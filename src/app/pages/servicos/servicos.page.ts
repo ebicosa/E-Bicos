dd28b39a764a10db982dc6b48b79761def110983
AlfredoRodrigo <alfredorodrigo60@gmail.com>
2021-06-01 21:37:05 -0300

    Atualizando tela de serviços (tela home, no figma)

--- src/app/pages/servicos/servicos.page.ts
+++ src/app/pages/servicos/servicos.page.ts
@@ -1,4 +1,6 @@
+import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { range } from 'rxjs';
 
 @Component({
   selector: 'app-servicos',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicosPage implements OnInit {
 
-  constructor() { }
+  cards = [
+    {
+      categoria:"Construção civil",
+      subcategoria: "Pedreiro",
+      descricao: "Preciso de pedreiro para construir duas paredes.",
+      cidade: "Campina Grande",
+      estado: "PB",
+      data: "24/01/2021",
+      favorito: false,
+      id: 1
+    },
+    {
+      categoria:"Serviços domésticos",
+      subcategoria: "Babá",
+      descricao: "Preciso de babá para cuidar de criança hoje a noite.",
+      cidade: "Esperança",
+      estado: "PB",
+      data: "26/01/2021",
+      favorito: false,
+      id: 2
+    },
+    {
+      categoria:"Assistência técnica",
+      subcategoria: "Geladeira",
+      descricao: "Preciso de técnico para consertar geladeira com defeito.",
+      cidade: "Campina Grande",
+      estado: "PB",
+      data: "28/01/2021",
+      favorito: false,
+      id: 3
+    },
+    {
+      categoria:"Eventos",
+      subcategoria: "Segurança",
+      descricao: "Preciso de segurança para trabalhar em festa privada.",
+      cidade: "Remígio",
+      estado: "PB",
+      data: "26/01/2021",
+      favorito: false,
+      id: 4
+    },
+  ];
+
+  constructor(private navCtrl : NavController) { }
 
   ngOnInit() {
   }
+
+  showPageFiltros() {
+    this.navCtrl.navigateForward('filtros')
+  }
+
+  alteraFavorito(card : any) {
+    this.cards.forEach(element => {
+      if (element.id == card.id) {
+        element.favorito = !element.favorito;
+      }
+    });
+  }
+
 
 }
