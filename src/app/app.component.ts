c34b389d9775f1af61efc183e46389c746bb97c1
AlfredoRodrigo <alfredorodrigo60@gmail.com>
2021-06-02 18:01:51 -0300

    Adicionando tela de menu e atualizando tela de serviços

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  nome_de_perfil = "Victor Emanuel";
+  e_mail = "nome.sobrenome@servidor.com";
+
+  public appPages = [
+    { title: 'Postar serviço', url: '', icon: 'briefcase-outline' },
+    { title: 'Minha conta', url: 'perfil', icon: 'person-circle-outline' },
+    { title: 'Favoritos', url: '', icon: 'heart-outline' },
+    { title: 'Central de ajuda', url: '', icon: '' },
+    { title: 'Sobre o E-Bicos', url: '', icon: '' },
+    { title: 'Dicas de segurança', url: '', icon: '' },
+    { title: 'Termos de uso', url: '', icon: '' },
+  ];
+  
   constructor() {}
 }
