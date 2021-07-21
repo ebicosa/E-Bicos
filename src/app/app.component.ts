3b130d869289639f66bd4d782a98256e6550aaae
Bruno <brunoob14540@gmail.com>
2021-07-21 20:43:47 -0300

    correção das paginas quem-somos e termos-de-uso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
 
-  private nome_de_perfil:string;
-  private e_mail:string;
+  private nome_de_perfil: string;
+  private e_mail: string;
 
   public appPages = [
     { title: 'Postar serviço', url: 'inserir-servico', icon: 'briefcase-outline' },
     { title: 'Minha conta', url: 'perfil', icon: 'person-circle-outline' },
     { title: 'Favoritos', url: 'favoritos', icon: 'heart-outline' },
     { title: 'Central de ajuda', url: 'central-ajuda', icon: '' },
-    { title: 'Sobre o E-Bicos', url: '', icon: '' },
+    { title: 'Sobre o E-Bicos', url: 'quem-somos', icon: '' },
     { title: 'Dicas de segurança', url: '', icon: '' },
-    { title: 'Termos de uso', url: '', icon: '' },
+    { title: 'Termos de uso', url: 'termos-de-uso', icon: '' },
   ];
-  
+
   constructor() {
-    if(localStorage.getItem("profile_name") === null){
-      localStorage.setItem("profile_name", "Victor Emanuel");
+    if(localStorage.getItem('profile_name') === null){
+      localStorage.setItem('profile_name', 'Victor Emanuel');
     }
-    this.nome_de_perfil = localStorage.getItem("profile_name");
-    if(localStorage.getItem("e_mail") === null){
-      localStorage.setItem("e_mail", "nome.sobrenome@gmail.com");
+    this.nome_de_perfil = localStorage.getItem('profile_name');
+    if(localStorage.getItem('e_mail') === null){
+      localStorage.setItem('e_mail', 'nome.sobrenome@gmail.com');
     }
-    this.e_mail = localStorage.getItem("e_mail");
+    this.e_mail = localStorage.getItem('e_mail');
   }
   ngOnInit() {
-    localStorage.setItem("subcategorias", JSON.stringify([{nome: "Eletricista"},{nome: "Encanador"}]));
-    localStorage.setItem("categorias", JSON.stringify([{nome: "Construção Civil"},{nome: "Serviços domésticos"}]));
-    localStorage.setItem("estados", JSON.stringify( [{nome: "Paraíba"}, { nome: "Acre"}]));
-    localStorage.setItem("cidades", JSON.stringify([{nome: "Campina Grande"}, { nome: "Rio Branco"}]));
-    localStorage.setItem("faixasPrecos", JSON.stringify( [{minimo: 100, maximo: 1000},{minimo: 1000, maximo:5000}]));
+    localStorage.setItem('subcategorias', JSON.stringify([{nome: 'Eletricista'},{nome: 'Encanador'}]));
+    localStorage.setItem('categorias', JSON.stringify([{nome: 'Construção Civil'},{nome: 'Serviços domésticos'}]));
+    localStorage.setItem('estados', JSON.stringify( [{nome: 'Paraíba'}, { nome: 'Acre'}]));
+    localStorage.setItem('cidades', JSON.stringify([{nome: 'Campina Grande'}, { nome: 'Rio Branco'}]));
+    localStorage.setItem('faixasPrecos', JSON.stringify( [{minimo: 100, maximo: 1000},{minimo: 1000, maximo:5000}]));
   }
-  
+
 }
